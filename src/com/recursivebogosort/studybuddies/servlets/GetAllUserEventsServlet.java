1d8966c559b74417e51f214a64e1d4cd808b1d30
Farhan Ali <farhan94@gmail.com>
2016-11-25 15:35:44 -0600

    get events for user servlet

--- src/com/recursivebogosort/studybuddies/servlets/GetAllUserEventsServlet.java
+++ src/com/recursivebogosort/studybuddies/servlets/GetAllUserEventsServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
@@ -17,10 +16,12 @@ import com.google.appengine.labs.repackaged.org.json.JSONArray;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
 import com.googlecode.objectify.Ref;
+import com.recursivebogosort.studybuddies.entities.Event;
 import com.recursivebogosort.studybuddies.entities.GroupMember;
 import com.recursivebogosort.studybuddies.entities.StudyBuddiesUser;
 
-public class GetStudyBuddiesUserServlet extends HttpServlet{
+public class GetAllUserEventsServlet extends HttpServlet {
+
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
@@ -33,33 +34,20 @@ public class GetStudyBuddiesUserServlet extends HttpServlet{
 			Ref<StudyBuddiesUser> sbuRef = ofy().load().type(StudyBuddiesUser.class).id(id);
 			StudyBuddiesUser sbu = sbuRef.get();
 			if(sbu != null){
-				JSONObject jo = new JSONObject();
-				try {
-					jo.put("uid", sbu.getId());
-					jo.put("name", sbu.getName());
-					jo.put("email", sbu.getEmail());
-					jo.put("phone_number", sbu.getPhoneNumber());
-					jo.put("email_notify", sbu.isSubscribedToEmails());
-					jo.put("sms_notify",sbu.isSubscribedToSMS());
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+
 				 JSONArray ar = new JSONArray();
-				 ar.put(jo);
-				 ArrayList<Ref<GroupMember>> groups = sbu.getAllGroups();
-				 for(int i = 0; i < groups.size(); i++){
-					 Ref<GroupMember> gmRef = groups.get(i);
-					 GroupMember gm = gmRef.get();
-					 jo = new JSONObject();
+				 ArrayList<Ref<Event>> events = sbu.getEvents();
+				 for(int i = 0; i < events.size(); i++){
+					 Ref<Event> eventRef = events.get(i);
+					 Event ev = eventRef.get();
+					 JSONObject jo = new JSONObject();
 					 try {
-						jo.put("group_uid", gm.getGroup().getId());
-						if(gm.getGroup().getOwner().getUser().getId() == sbu.getId()){
-							jo.put("is_owner", true);
-						}
-						else{
-							jo.put("is_owner", false);
-						}
+						jo.put("event_uid", ev.getId());
+						jo.put("event_date", ev.getDate());
+						jo.put("event_name", ev.getEventName());
+						jo.put("event_location", ev.getEventLocation());
+						jo.put("event_description", ev.getEventDescription());
+
 					} catch (JSONException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -78,5 +66,5 @@ public class GetStudyBuddiesUserServlet extends HttpServlet{
 		
 
 	}
-
+	
 }
